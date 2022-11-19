3d84159d558328f4933ed9ba55022298c377a86a
Umar Fauque Imran <18-36115-1@student.aiub.edu>
2022-11-19 21:13:32 +0600

    Strings Python

--- Basics/boolean_data_type.py
+++ Basics/boolean_data_type.py
@@ -1,4 +1,8 @@
-x = 10
+x = 0
 y = 5
 
-print(x > y)
+# print(x > y)
+
+print(bool(x))
+
+# basically, it returns whether it holds a value or not by true or false
---
+++ Basics/strings_python.py
@@ -0,0 +1,26 @@
+#strings in python
+# x = "This 'is' a String"
+# y = 'This is a "string" in single quote' # within single quotes we can use double quotes and vice versa
+# z = 'This is a \'string\' in single-single quote' # within single quotes we can use single quotes using escape character which is '\' backslash
+
+# print(x)
+# print(x[1]) #printing 'h' from x, cause strings are array
+# print(y)
+# print(z)
+
+#multiline Strings
+# p = """This is a multiline
+# String
+# """
+# q ='''This is also a multiline String
+# '''
+# print(p)
+# print(q)
+
+#membership operators
+p = """This is a multiline 
+String
+"""
+q ='''This is also a multiline String
+'''
+print('is' in q)
