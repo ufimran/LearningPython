89abbb055bbe330fba9bad3c1a03880f25106ccb
Umar Fauque Imran <18-36115-1@student.aiub.edu>
2022-11-19 15:30:37 +0600

    Arithmetic Operators

---
+++ Basics/operators_in_python.py
@@ -0,0 +1,21 @@
+#Arithmetic operators
+# x=15
+# y=4
+#
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x%y) #remainder
+# print(x**y) #ex potential
+# print(x//y) #floor division, round the result to nearest full number
+
+
+#Assignment operators
+
+
+#Comparison operators
+#Logical operators
+#Identity operators
+#Membership operators
+#Bitwise operators
