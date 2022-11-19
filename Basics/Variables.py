aa4276b6b9d58c1ee9404c26dab73a03a57a264e
Umar Fauque Imran <18-36115-1@student.aiub.edu>
2022-11-19 15:07:09 +0600

    second commit

--- Basics/Variables.py
+++ Basics/Variables.py
@@ -1,2 +1,13 @@
 username="Umar Faruque"
-print(username)
+print(username) #no need to specify data type in python
+#print(190)
+
+#print(type(username))  #Finding the Variable Type
+
+x=600 #initial value
+print(x)
+#x=800 #changed value
+#print(x) #prints the both x values
+
+y=x #reassigning another value
+print(y)
