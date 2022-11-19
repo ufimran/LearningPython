25a71e3d1acfe97163a995ab8d0697e67c24c549
Umar Fauque Imran <18-36115-1@student.aiub.edu>
2022-11-19 15:19:02 +0600

    Varible Rules 1

---
+++ Basics/PythonVariablesRules.py
@@ -0,0 +1,21 @@
+#_test = "Umar_Faruque" #can't start a variable with Number, but with letter or underscore
+#print(_test)
+
+
+#uf69_test=4862 #we can use number after letter or underscore in variable naming
+#print(uf69_test)
+
+
+#UF69_test=5896 #variables are case sensitive
+#uf69_test=9875
+#print(uf69_test)
+#print(UF69_test)
+
+
+#reserved keywords from python documentation can't be used as a variable
+#return= 58653
+#print(return) #won't print
+
+returnn=64564
+print(returnn) #will print
+
