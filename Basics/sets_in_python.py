76b1390fc36afdf80c9c57332d5c030c7059a5ca
Umar Fauque Imran <18-36115-1@student.aiub.edu>
2022-11-22 12:29:16 +0600

    Sets in Python

--- Basics/sets_in_python.py
+++ Basics/sets_in_python.py
@@ -1,5 +1,10 @@
-demo_set1 = {10,20,30,40}
+demo_set1 = {5,10,10,20,20,30,30,40}
 demo_set2 = {10,20,"30",40.5}
 demo_set3 = {"10", "30"}
+demo_set4 = set(("45",45,"67",67))
 
 print(demo_set1)
+print(demo_set4)
+print(len(demo_set1))
+
+print(20 in demo_set1) # checks whether value is availble or not by true and false
